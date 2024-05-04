36369bd58579d3312cce24730645d96e94d3ba3b
Aditya5009 <138042821+Aditya5009@users.noreply.github.com>
2024-05-05 00:53:34 +0530

    Assignment 9 Q1

---
+++ Assignment 9/Q1/StringReverse.java
@@ -0,0 +1,23 @@
+package com.sunbeam;
+
+import java.util.Scanner;
+
+public class StringReverse {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a string: ");
+		String str = sc.nextLine();
+		String reversed = reverseString(str);
+		System.out.println("Original string: " + str);
+		System.out.println("Reversed string: " + reversed);
+	}
+
+	public static String reverseString(String str) {
+		StringBuilder reversed = new StringBuilder();
+		for (int i = str.length() - 1; i >= 0; i--) {
+			reversed.append(str.charAt(i));
+		}
+		return reversed.toString();
+	}
+}
